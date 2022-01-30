83f0e05c68b458bd5a98265446da6de50748ea38
Sam Shull <sam.gh@samshull.com>
2022-01-30 16:26:48 -0700

    bug fix for caller file included recursively

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { dirname, resolve } from 'node:path';
+import { dirname, resolve, basename } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { promisify } from 'node:util';
 
@@ -12,6 +12,7 @@ export default async function importer(url, options = {}) {
     const promises = [];
     const path = fileURLToPath(url);
     const root = dirname(path);
+    const caller = basename(path);
     const {
       glob = promisedGlob,
       extensions = ['.mjs', '.cjs', '.js'],
@@ -22,8 +23,8 @@ export default async function importer(url, options = {}) {
     const pattern = await template(extensions);
     const files = await glob(pattern, { cwd: root });
     for (const file of files) {
-        if (file !== path && !exclude?.includes(file))
-            promises.push(add(root, file, results, collector));
+      if (file !== caller && !exclude?.includes(file))
+          promises.push(add(root, file, results, collector));
     }
     await Promise.all(promises);
     return results;
